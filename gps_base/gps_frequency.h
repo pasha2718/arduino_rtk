19d01138d59d83b8a5b66dcb30c1fedd4661cd49
Pasha Roberts <pasha@talentanalytics.com>
2020-07-24 11:20:39 -0400

    First take at issues 6 and 7 renamed git-version.h to git_version.h set up gps_frequency.h for gps_base but not others yet set survey-in to 3 hours changed survey-in display to not get lat/long every 2 seconds or at all changed survey-in min accuracy to 3 from 5 improved comments

---
+++ gps_base/gps_frequency.h
@@ -0,0 +1,3 @@
+// The broadcast frequency can be anywhere from 902-928MHz
+// change it here and it changes on oll modules due to symlinks
+const float FREQUENCY = 921.2;    // frequency in Mhz for broadcast
